ecb39ead57b6fcc072342dfee84fa09bf5e9e949
Marcin Lamberski <marcin.lamberski@gmail.com>
2024-02-29 17:49:35 +0100

    Initial commit

---
+++ src/main/java/com/findmyflight/findmyflight/FindmyflightApplication.java
@@ -0,0 +1,13 @@
+package com.findmyflight.findmyflight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindmyflightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindmyflightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/findmyflight/findmyflight/FindmyflightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findmyflight.findmyflight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindmyflightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
