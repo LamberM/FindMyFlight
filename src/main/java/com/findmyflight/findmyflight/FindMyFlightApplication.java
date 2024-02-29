62dccb6d5ddbe82991832f42890b5db6ec2dbf52
Marcin Lamberski <marcin.lamberski@gmail.com>
2024-02-29 17:49:35 +0100

    Initial commit

---
+++ src/main/java/com/findmyflight/findmyflight/FindMyFlightApplication.java
@@ -0,0 +1,13 @@
+package com.findmyflight.findmyflight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMyFlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMyFlightApplication.class, args);
+	}
+
+}
