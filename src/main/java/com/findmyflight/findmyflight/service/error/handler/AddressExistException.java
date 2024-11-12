a225863de5ff4d831a2721cebbeafd8a07bb5f92
Marcin Lamberski <marcin.lamberski@gmail.com>
2024-11-12 18:36:53 +0100

    deleted

--- src/main/java/com/findmyflight/findmyflight/service/error/handler/AddressExistException.java
+++
@@ -1,7 +0,0 @@
-package com.findmyflight.findmyflight.service.error.handler;
-
-public class AddressExistException extends RuntimeException{
-    public AddressExistException() {
-        super("Address exists");
-    }
-}
--- src/main/java/com/findmyflight/findmyflight/service/error/handler/IdNotExistException.java
+++
@@ -1,7 +0,0 @@
-package com.findmyflight.findmyflight.service.error.handler;
-
-public class IdNotExistException extends RuntimeException{
-    public IdNotExistException() {
-        super("ID does not exist");
-    }
-}
