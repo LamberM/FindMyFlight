ba2a9490ffa3bd2a06509788220abbb0ec6ed884
Marcin Lamberski <marcin.lamberski@gmail.com>
2024-11-12 18:42:28 +0100

    updated repository class

--- src/main/java/com/findmyflight/findmyflight/service/emailnotificationreceiver/EmailNotificationReceiverRepository.java
+++ src/main/java/com/findmyflight/findmyflight/service/emailnotificationreceiver/EmailNotificationReceiverRepository.java
@@ -1,9 +1,6 @@
 package com.findmyflight.findmyflight.service.emailnotificationreceiver;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
-public interface EmailNotificationReceiverRepository extends JpaRepository<EmailNotificationReceiver,Long> {
-    EmailNotificationReceiver findByAddress(@Param("address") String address);
-    boolean existsByAddress(@Param("address") String address);
+public interface EmailNotificationReceiverRepository extends JpaRepository<EmailNotificationReceiver, Long> {
 }
