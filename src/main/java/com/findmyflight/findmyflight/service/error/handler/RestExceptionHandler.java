0fbd83b19a0401d390c1519257431980651e4f9a
Marcin Lamberski <marcin.lamberski@gmail.com>
2024-11-12 18:38:36 +0100

    deleted useless method

--- src/main/java/com/findmyflight/findmyflight/service/error/handler/RestExceptionHandler.java
+++ src/main/java/com/findmyflight/findmyflight/service/error/handler/RestExceptionHandler.java
@@ -13,16 +13,4 @@ public class RestExceptionHandler {
         return ResponseEntity.badRequest()
                 .body(new RestErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
     }
-
-    @ExceptionHandler(value = {AddressExistException.class})
-    protected ResponseEntity<RestErrorResponse> handleAddressExistException(AddressExistException e) {
-        return ResponseEntity.badRequest()
-                .body(new RestErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
-    }
-
-    @ExceptionHandler(value = {IdNotExistException.class})
-    protected ResponseEntity<RestErrorResponse> handleIdNotExistException(IdNotExistException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(new RestErrorResponse(HttpStatus.NOT_FOUND.value(), e.getMessage()));
-    }
 }
