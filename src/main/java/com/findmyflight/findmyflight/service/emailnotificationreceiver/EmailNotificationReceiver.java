974d2b290ea534673df1269b13ad590731ea2107
Marcin Lamberski <marcin.lamberski@gmail.com>
2024-11-21 20:18:09 +0100

    added @Column(unique = true)

--- src/main/java/com/findmyflight/findmyflight/service/emailnotificationreceiver/EmailNotificationReceiver.java
+++ src/main/java/com/findmyflight/findmyflight/service/emailnotificationreceiver/EmailNotificationReceiver.java
@@ -1,5 +1,6 @@
 package com.findmyflight.findmyflight.service.emailnotificationreceiver;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,6 +16,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class EmailNotificationReceiver {
     @Id
+    @Column(unique = true)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
