de175f1e8c86b8b9831e18be176de8158900e807
Marcin Lamberski <marcin.lamberski@gmail.com>
2025-01-19 12:47:52 +0100

    added @Profile and changed class to abstract

--- src/test/java/com/findmyflight/findmyflight/IntegrationTest.java
+++ src/test/java/com/findmyflight/findmyflight/IntegrationTest.java
@@ -7,13 +7,15 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+@ActiveProfiles("test")
 @AutoConfigureWebTestClient
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
-public class IntegrationTest {
+public abstract class IntegrationTest {
 
     @Autowired
     protected WebTestClient webTestClient;
