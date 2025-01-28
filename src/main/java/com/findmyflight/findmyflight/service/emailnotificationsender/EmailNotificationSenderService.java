4ec35809785aaa3d204503f4fe824ea6bc9476c4
Marcin Lamberski <marcin.lamberski@gmail.com>
2025-01-28 18:35:08 +0100

    updated

--- src/main/java/com/findmyflight/findmyflight/service/emailnotificationsender/EmailNotificationSenderService.java
+++ src/main/java/com/findmyflight/findmyflight/service/emailnotificationsender/EmailNotificationSenderService.java
@@ -1,11 +1,12 @@
 package com.findmyflight.findmyflight.service.emailnotificationsender;
 
-import com.findmyflight.findmyflight.service.flightwatcher.FlightWatcher;
-import com.findmyflight.findmyflight.service.flightwatcher.FlightWatcherRepository;
+import com.findmyflight.findmyflight.service.emailnotificationreceiver.EmailNotificationReceiver;
+import com.findmyflight.findmyflight.service.emailnotificationreceiver.EmailNotificationReceiverRepository;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
@@ -14,18 +15,19 @@ import org.thymeleaf.spring6.SpringTemplateEngine;
 @RequiredArgsConstructor
 public class EmailNotificationSenderService {
     private final JavaMailSender mailSender;
-    private final FlightWatcherRepository flightWatcherRepository;
+    private final EmailNotificationReceiverRepository emailNotificationReceiverRepository;
     private final SpringTemplateEngine springTemplateEngine;
 
+    @Scheduled(cron = "0 0 0 * * *")
     public void sendMailToSubscribers() throws MessagingException {
         var context = new Context();
-        var flightWatchers = flightWatcherRepository.findSuspendedFlightWatchers();
-        for (FlightWatcher flightWatcher : flightWatchers) {
-            context.setVariable("flightWatchers", flightWatchers);
+        var emailNotificationReceivers = emailNotificationReceiverRepository.findEmailNotificationReceiversWithActiveFlightWatchers();
+        for (EmailNotificationReceiver emailNotificationReceiver : emailNotificationReceivers) {
+            context.setVariable("flightWatchers", emailNotificationReceiver.getFlightWatchers());
             var htmlContent = springTemplateEngine.process("email-template", context);
             var message = mailSender.createMimeMessage();
             var helper = new MimeMessageHelper(message, true);
-            helper.setTo(flightWatcher.getEmailNotificationReceiver().getAddress());
+            helper.setTo(emailNotificationReceiver.getAddress());
             helper.setSubject("Your observed flights in SkyDealHunter");
             helper.setText(htmlContent, true);
             mailSender.send(helper.getMimeMessage());
