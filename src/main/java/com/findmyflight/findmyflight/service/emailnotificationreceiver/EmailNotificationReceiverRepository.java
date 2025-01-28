31a576baa633238793b7c20b72ab7cdf7f8b789e
Marcin Lamberski <marcin.lamberski@gmail.com>
2025-01-28 18:32:17 +0100

    created new queries

--- src/main/java/com/findmyflight/findmyflight/service/emailnotificationreceiver/EmailNotificationReceiverRepository.java
+++ src/main/java/com/findmyflight/findmyflight/service/emailnotificationreceiver/EmailNotificationReceiverRepository.java
@@ -1,6 +1,14 @@
 package com.findmyflight.findmyflight.service.emailnotificationreceiver;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmailNotificationReceiverRepository extends JpaRepository<EmailNotificationReceiver, Long> {
+    @Query("select e from EmailNotificationReceiver e join fetch e.flightWatchers fw where fw.suspended = false")
+    List<EmailNotificationReceiver> findEmailNotificationReceiversWithActiveFlightWatchers();
+
+    @Query("select e from EmailNotificationReceiver e  where e.address = ?1")
+    EmailNotificationReceiver findEmailNotificationReceiverByAddress(String address);
 }
