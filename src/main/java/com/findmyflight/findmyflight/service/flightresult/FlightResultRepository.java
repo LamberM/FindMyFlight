654e5a15117cf4ec30060332063c036d7c0b7bc5
Marcin Lamberski <marcin.lamberski@gmail.com>
2025-02-10 19:29:08 +0100

    created EmailNotificationSenderService with cron and revelant changes in existing classes

---
+++ src/main/java/com/findmyflight/findmyflight/service/flightresult/FlightResultRepository.java
@@ -0,0 +1,6 @@
+package com.findmyflight.findmyflight.service.flightresult;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FlightResultRepository extends JpaRepository<FlightResult, Long> {
+}
