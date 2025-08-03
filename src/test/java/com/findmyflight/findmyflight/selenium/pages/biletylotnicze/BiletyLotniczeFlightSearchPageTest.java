f0da6e31b0337d107e5d26a09bf65b16e9965b5e
Marcin Lamberski <marcin.lamberski@gmail.com>
2025-08-03 13:17:24 +0200

    Disabled test

--- src/test/java/com/findmyflight/findmyflight/selenium/pages/biletylotnicze/BiletyLotniczeFlightSearchPageTest.java
+++ src/test/java/com/findmyflight/findmyflight/selenium/pages/biletylotnicze/BiletyLotniczeFlightSearchPageTest.java
@@ -4,12 +4,14 @@ import com.findmyflight.findmyflight.UnitTest;
 import com.findmyflight.findmyflight.data.scrape.FlightJourney;
 import com.findmyflight.findmyflight.service.flightwatcher.FlightWatcherResponse;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.LocalDate;
 
+@Disabled
 class BiletyLotniczeFlightSearchPageTest implements UnitTest {
     // TODO this is just a dummy test used to debug actual implementation - to be removed
 
