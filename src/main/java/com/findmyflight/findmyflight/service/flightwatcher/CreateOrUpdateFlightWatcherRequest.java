aa70186efcc838a711b3f36202d5a9a522e571ef
mlamber <marcin.lamberski@gmail.com>
2025-10-08 19:20:59 +0200

    added new fields in FlightWatcher classes

--- src/main/java/com/findmyflight/findmyflight/service/flightwatcher/CreateOrUpdateFlightWatcherRequest.java
+++ src/main/java/com/findmyflight/findmyflight/service/flightwatcher/CreateOrUpdateFlightWatcherRequest.java
@@ -1,9 +1,12 @@
 package com.findmyflight.findmyflight.service.flightwatcher;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Builder
@@ -13,6 +16,8 @@ public record CreateOrUpdateFlightWatcherRequest(
         LocalDate fromDate,
         LocalDate toDate,
         @NotNull Boolean suspended,
+        @DecimalMin(value = "1", message = "maxPrice should be more than 0") BigDecimal maxPrice,
+        @NotNull @Min(1) Integer maxFlightCount,
         @NotNull Long emailNotificationReceiverId
 ) {
 }
